7611261b3b4a79b75adbf8cdd8f582e2d865b69a
NaFaPaz <nfanego@gmail.com>
2021-12-27 22:47:57 -0300

    fix responsive

---
+++ script.js
@@ -0,0 +1,39 @@
+const navBar = document.querySelector('.nav-bar');
+const menuBtn = document.querySelectorAll('.toggle');
+
+const handleMenu = () => {    
+    if (navBar.style.display === 'flex'){
+        navBar.style.display = 'none';
+    } else {
+        navBar.style.display = 'flex';
+    }
+    
+}
+
+const checkSize = () => {
+    
+    var w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+    /* var h = window.innerHeight; */
+    var targetWidth = 740;
+    /* var targetHeight = 600; */
+    
+     
+    if (/* (h > targetHeight) && */ (w > targetWidth)){
+        navBar.style.display = 'flex';
+        for (element of menuBtn) {
+            element.removeEventListener('click', handleMenu)
+            } 
+           
+    } else {
+    
+        
+    for (element of menuBtn) {
+            navBar.style.display = 'none';
+            element.addEventListener('click', handleMenu)
+            };
+    }
+
+}
+
+/* window.addEventListener('load', checkSize) */
+window.addEventListener('resize', checkSize)
